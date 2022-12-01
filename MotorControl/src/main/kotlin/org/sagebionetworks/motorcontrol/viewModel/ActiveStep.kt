6a17a92376546acb9fb2c23b639cf7274e665ded
Aaron Rabara <aaron.rabara@sagebase.org>
2022-12-01 13:35:28 -0800

    Changed recorder name to be just the identifier

--- MotorControl/src/main/kotlin/org/sagebionetworks/motorcontrol/viewModel/ActiveStep.kt
+++ MotorControl/src/main/kotlin/org/sagebionetworks/motorcontrol/viewModel/ActiveStep.kt
@@ -74,7 +74,7 @@ interface ActiveStep {
             listOf(
                 RecorderScheduledAssessmentConfig(
                     recorder = BackgroundRecordersConfigurationElement.Recorder(
-                        "${identifier}/${hand?.name?.lowercase()}",
+                        "$identifier",
                         MotionRecorderConfiguration.TYPE
                     ),
                     disabledByAppForTaskIdentifiers = setOf(),
